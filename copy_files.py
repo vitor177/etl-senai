c0b70c27300c901c642fe565a14479afb712b6a4
João Vítor Fonseca de Mendonça <joovitor177@gmail.com>
2024-10-14 15:34:29 -0300

    Update copy_files.py

--- copy_files.py
+++ copy_files.py
@@ -3,8 +3,8 @@ import os
 
 
 def copia():
-    network_drive = "Z:/"
-    #network_path = "C:\Users\joaomendonca\Documents\senai-extract\copy_files.py"
+    network_drive = "D:/ESTAÇÕES ISI-ER/"
+    network_path = "D:/joaovitor-ws/senai-extract"
 
     seg_directory = "./raw/seg"
     min_directory = "./raw/min"
@@ -16,19 +16,25 @@ def copia():
     estacoes_segundo = []
     estacoes_minuto = []
 
-    for filename in os.listdir(network_drive):
-        if filename.endswith(".dat"):
-            print(f"Copiando o arquivo {filename}")
-            full_path = os.path.join(network_drive, filename)
-            if "seg" in filename:
-                estacoes_segundo.append(full_path)
-                shutil.copy(full_path, seg_directory)
-            else:
-                estacoes_minuto.append(full_path)
-                shutil.copy(full_path, min_directory)
 
-    assert len(estacoes_minuto) == len(estacoes_segundo)
+    for root, dirs, files in os.walk(network_drive):
+        #print(files)
+        for file in files:
+            
+            #if file.endswith("ATLAS-RN PAU DOS FERROS_GHI1_seg_PauFerros.dat.backup"):
+            if file.endswith(".dat"):
+                print(f"Copiando o arquivo {file}")
+                full_path = os.path.join(network_drive, root, file)
+                if "NATAL" not in file: #and "LAJES" not in file:
+                    print(f"Copiando o arquivo: {file}")
+                    if "seg" in file:
+                        estacoes_segundo.append(full_path)
+                        shutil.copy(full_path, seg_directory)
+                    else:
+                        estacoes_minuto.append(full_path)
+                        shutil.copy(full_path, min_directory)
 
-    print("Arquivos copiados com sucesso!")
 
 
+if _name=="main_":
+    copia()
